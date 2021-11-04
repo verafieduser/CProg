99bc80a0e935532b1c1419383a6fcc13b6d20ff9
verafied <verasolnygren@gmail.com>
2021-11-04 20:20:39 +0100

    using structure provided by lecturer instead

---
+++ test/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* Om SDL2 används!
+// Alla dessa SDL inkluderingsfiler används inte i detta testprogram.
+// Bifogas endast för test av SDL installation! 
+#include <SDL2/SDL.h> 
+#include <SDL2/SDL_image.h> 
+#include <SDL2/SDL_mixer.h> 
+#include <SDL2/SDL_ttf.h>
+*/
+
+// Global variable containing the Path to Your resources folder.
+// usage ex. cout << (resPath + "images/gubbe.jpg").c_str() 
+//std::string resPath = "../../resources/"; 
+
+
+int main(int argc, char* argv[]) {
+
+	std::cout << "Empty" << std::endl;
+
+
+	return 0;	
+}
